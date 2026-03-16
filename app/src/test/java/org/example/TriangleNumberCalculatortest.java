8c5ff6a224fbd474d450df1d61042ec0ee255aae
Droman817 <rd14409@mail.harpercollege.edu>
2026-03-16 04:18:30 +0000

    add generalized input validation utility and driver program

---
+++ app/src/test/java/org/example/TriangleNumberCalculatortest.java
@@ -0,0 +1,37 @@
+package trianglenumbers;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TriangleNumberCalculatorTest {
+
+    @Test
+    void testValue() {
+
+        TriangleNumberCalculator calc = new TriangleNumberCalculator();
+
+        assertEquals(1, calc.value(1));
+        assertEquals(3, calc.value(2));
+        assertEquals(10, calc.value(4));
+    }
+
+    @Test
+    void testAdd() {
+
+        TriangleNumberCalculator calc = new TriangleNumberCalculator();
+
+        assertEquals(2, calc.add(1,1));
+        assertEquals(9, calc.add(2,3));
+        assertEquals(13, calc.add(4,2));
+    }
+
+    @Test
+    void testSubtract() {
+
+        TriangleNumberCalculator calc = new TriangleNumberCalculator();
+
+        assertEquals(0, calc.subtract(1,1));
+        assertEquals(-3, calc.subtract(2,3));
+        assertEquals(7, calc.subtract(4,2));
+    }
+}
